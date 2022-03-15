0fbb6ae04f5ea4bc42340b16e2557505d96bf965
Sania Shinde <101562128+Sania2001@users.noreply.github.com>
2022-03-15 15:22:19 +0530

    Update Calculator.java

--- Calculator.java
+++ Calculator.java
@@ -1,2 +1,18 @@
+package com.company;
+import java.sql.SQLOutput;
+import java.util.Scanner;
 public class Calculator {
+
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+        int n1,n2,Total;
+        System.out.println("Enter n1");
+        n1 = input.nextInt();
+        System.out.println("Enter n2");
+        n2 = input.nextInt();
+        Total = n1+n2;
+        System.out.println(Total);
+
+    }
 }
